23da5634bce0c18c0c62078c98a2f3513f4c552a
srfmiku <848918988@QQ.com>
2025-01-16 22:00:01 +0800

    Site updated: 2025-01-16 22:00:00

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/16/hello-world/"];function toRandomPost(){
+var posts=["2025/01/16/hello-world/","2025/01/16/start/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
