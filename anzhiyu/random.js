60f138f5c7cedbf49a86563fa64a54487c3c690b
srfmiku <848918988@QQ.com>
2025-01-20 17:40:34 +0800

    Site updated: 2025-01-20 17:40:32

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/17/好物/","2025/01/16/日记/","2025/01/16/hello-world/","2025/01/17/开发过程遇到的问题与解决/"];function toRandomPost(){
+var posts=["2025/01/17/好物/","2025/01/16/日记/","2025/01/16/hello-world/","2025/01/17/开发过程遇到的问题与解决/","2025/01/17/番剧相关/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
